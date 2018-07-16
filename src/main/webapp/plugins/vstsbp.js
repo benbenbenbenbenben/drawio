add53157a2a99f7604f672e5e7ff9fcc6e7b7e99
Benjamin Babik <work@benjaminbabik.com>
2018-07-16 15:05:14 +0100

    doc loading in edit mode

--- src/main/webapp/plugins/vstsbp.js
+++ src/main/webapp/plugins/vstsbp.js
@@ -1,11 +1,27 @@
 
 Draw.loadPlugin(function(ui) {
     window.ui = ui
-    window.parent.postMessage({
-        action: "notify",
-        parameters: [
-            "pluginReady"
-        ]
-    }, '*')
-    
+    const EDITMODE = window.ui.editor.editable
+
+    window.onmessage = function(e) {        
+        if (e.data.namespace && e.data.namespace == "vstsbp") {
+            switch(e.data.action) {
+                case "load":
+                window.ui.loadFile(e.data.parameters[0])
+                break;
+                default:
+                break;
+            }
+        }
+    }
+
+    if (EDITMODE) {
+        window.parent.postMessage({
+            namespace: "vstsbp",
+            action: "notify",
+            parameters: [
+                "editPluginReady"
+            ]
+        }, '*')
+    }
 })
